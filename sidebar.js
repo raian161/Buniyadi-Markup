edf9428845f2ab64fbf6286c806ab2f08f31465c
raian <munsurtarik@gmail.com>
2020-11-16 15:31:29 +0600

    dropdown completed

--- sidebar.js
+++ sidebar.js
@@ -1,2 +1,18 @@
-var logo = document.querySelectorAll('.logo')
-console.log(logo);
+var cart = document.querySelector('#cart')
+var cartContent = document.querySelector('#cart-content')
+var bell = document.querySelector('#bell')
+var bellContent = document.querySelector('#bell-content')
+var user = document.querySelector('#user')
+var userContent = document.querySelector('#user-content')
+
+cart.addEventListener('click', () => {
+    cartContent.classList.toggle('open')
+})
+
+bell.addEventListener('click', () => {
+    bellContent.classList.toggle('open')
+})
+
+user.addEventListener('click', () => {
+    userContent.classList.toggle('open')
+})
